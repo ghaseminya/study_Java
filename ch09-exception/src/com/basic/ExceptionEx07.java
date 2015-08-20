b70fa8bd4f821bfdb4143353699a4e2cb7747c26
to2share <to2share@gmail.com>
2015-08-21 08:51:25 +0900

    modify

--- ch09-exception/src/com/basic/ExceptionEx07.java
+++ ch09-exception/src/com/basic/ExceptionEx07.java
@@ -1,19 +1,22 @@
 //예외처리 - 사용자 정의 예외 처리
-//
 //양수만 입력되게 하고 음수 입력시 예외처리하기
-//
+//--------------------------------
 //사용자 정의 예외 클래스 작성: 예외문구만 받아서 처리하기 위해 사용
 
-//사용자 정의 예외 클래스 정의
+
 package com.basic;
 
+//오노테이션 처리 방법
+//경고메시지가 나오는 구문에 마우스 커스를 가져간 다음 나오는 창에서 
+//Add @SuppressWarnings 'serial' to 'UserException' 링크 클릭
 
+//사용자 정의 예외 클래스 정의: 예외문구만 받아서 처리하기 위해 사용
+@SuppressWarnings("serial")
 class UserException extends Exception{
 	public UserException(String str){
 		super(str);
 	}
 }
-
 
 public class ExceptionEx07 {
 
@@ -21,8 +24,12 @@ public class ExceptionEx07 {
 
 //		UserException ux = new UserException(str);
 		
+//		int a = -11;
+		int a = 11;
+		
+		//양수만 입력되게 하고 음수 입력시 예외처리하기
 		try{
-			int a = -11;
+//			
 			if( a <=0 ){
 				//사용자가 정의한 예외를 인위적으로 발생시킴
 				//예외 발생시 UserException객체가 생성되고 해당 메시지가 전달됨
@@ -31,5 +38,17 @@ public class ExceptionEx07 {
 		}catch(UserException e){
 			System.out.println(e.getMessage());
 		}
+		
+		System.out.println(a);
 	}
 }
+
+
+/*//int a = -11; 경우 출력결과
+양수가 아닙니다
+-11
+*/
+
+/*//int a = 11; 경우 출력결과
+11
+*/
