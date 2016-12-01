5750d45e99756516b3f806f0b5ad0d03b758686b
codedragon <codedragon@tistory.com>
2016-12-01 18:20:07 +0900

    modify

--- ch07-array/src/ArrayEx17.java
+++ ch07-array/src/ArrayEx17.java
@@ -9,11 +9,13 @@ public class ArrayEx17 {
 		Arrays.sort(s);
 		System.out.println(Arrays.toString(s));
 		// [AAA, CCC, DDD, TTT, aaa, bbb, mmm]
+		
 
 		// TODO 2: 대소문자 구분 없이 정렬
 		Arrays.sort(s, String.CASE_INSENSITIVE_ORDER);
 		System.out.println(Arrays.toString(s));
 		// AAA, aaa, bbb, CCC, DDD, mmm, TTT]
+		//대문자 순 정렬 -> 소문자 순 정렬 -> 대문자 순 정렬 -> 소문자 순 정렬 -> 대문자 순 정렬
 	}
 }
 
--- ch07-array/src/ArrayEx20.java
+++ ch07-array/src/ArrayEx20.java
@@ -1,4 +1,5 @@
 //문자열 배열 내림차순 정렬(역순 소트)
+//객체 학습 후
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,18 +8,20 @@ import java.util.List;
 public class ArrayEx20 {
 
 	public static void main(String[] args) {
-		String[] a = { "bbb", "AAA", "DDD", "똠방각하", "CCC", "aaa" };
+		String[] a = { "flower", "dragon", "DRAGON", "코드드레곤", "CODE", "code" };
 
+		// String.CASE_INSENSITIVE_ORDER: 대소문자 구분 없이 정렬
 	    Arrays.sort(a, String.CASE_INSENSITIVE_ORDER);
 
-
-	//////////////////////////////////////////////////
+	    System.out.println(Arrays.toString(a));
+	    //[CODE, code, dragon, DRAGON, flower, 코드드레곤]
+	    //대문자 순 정렬 -> 소문자 순 정렬 -> 대문자 순 정렬 -> 한글 순 정렬
+	    
 	    // 배열을 리스트로 변환
 	    List<String> list = Arrays.asList(a);
 
 	    // 리스트 뒤집어 주기
 	    Collections.reverse(list);
-	//////////////////////////////////////////////////
 
 
 	    // 리스트를 배열로 다시 변환
