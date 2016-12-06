e5b96bc250082c2eaf02cec9d4984ab9f38e77cb
codedragon <codedragon@tistory.com>
2016-12-06 13:49:32 +0900

    modify

--- ch07-array/src/ArrayEx11.java
+++ ch07-array/src/ArrayEx11.java
@@ -20,6 +20,15 @@ public class ArrayEx11 {
 		for(int i=0; i<arr.length; i++)
 			System.out.printf("%d행의 길이: %d \n", i+1, arr[i].length);
 		
+		
+		//2차월 배열 출력
+		for( int i=0; i<arr.length; i++){ //행
+			
+			for(int j=0; j<arr[i].length; j++){ //열
+				System.out.println("arr[" + i + "][" + j +"] = "+ arr[i][j]);				
+			}	//j<arr[i].length : 이을 통해 2차원 배열은 둘이상의 1차원 배열을 묶은 형태라는 것을 알수 있습니다.
+		}
+		
 	}
 }
 
@@ -31,3 +40,20 @@ public class ArrayEx11 {
 //3행의 길이: 6 
 //4행의 길이: 3 
 //5행의 길이: 4 
+//arr[0][0] = 1
+//arr[1][0] = 2
+//arr[1][1] = 3
+//arr[2][0] = 1
+//arr[2][1] = 2
+//arr[2][2] = 3
+//arr[2][3] = 4
+//arr[2][4] = 5
+//arr[2][5] = 6
+//arr[3][0] = 4
+//arr[3][1] = 5
+//arr[3][2] = 6
+//arr[4][0] = 7
+//arr[4][1] = 8
+//arr[4][2] = 9
+//arr[4][3] = 10
+
